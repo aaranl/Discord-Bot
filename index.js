06185253a33c5ee3f88c9c45ea3b74c03c9edc7a
aaranl <66337295+aaranl@users.noreply.github.com>
2023-06-09 15:43:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Client, GatewayIntentBits } = require ('discord.js')
+const { token } = require('./config.json');
+
+
+const client = new Client ({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => { 
+    console.log('Ready!');
+
+})
+
+client.login(token);
