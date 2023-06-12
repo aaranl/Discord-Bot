8c18c0704f51ebbaed60b9ecb310318e8015b9fa
aaranl <66337295+aaranl@users.noreply.github.com>
2023-06-12 19:35:10 -0400

    third commit

--- index.js
+++ index.js
@@ -4,32 +4,42 @@ const { userInfo } = require('./database-objects.js')
 
 const client = new Client ({ intents: [GatewayIntentBits.Guilds] });
 
-client.once('ready', () => { 
-    console.log('Ready!');
+//Test to see if the bot is online
 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 })
+
 
 //***************************************** 
 
-client.on('message', (message) => {
-    //Ignore all other bots
-    if (message.author.bot) return;
+// client.on('message', (message) => {
+//     //Ignore all other bots
+//     if (message.author.bot) return;
 
-    if (message.author.id === userID) {
-        console.log('User ${message.authoer.username} sent a message')
+//     if (message.author.id === userID) {
+//         console.log(`User ${message.author.username} sent a message`)
 
-    }
+//     }
 
-})
+// })
+
+//****************************************** 
+
+
+
+//Slash command codes
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === 'messages') {
-
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
     }
 })
 
-//***************************************** 
+
+
+//Client login 
 
 client.login(token);
