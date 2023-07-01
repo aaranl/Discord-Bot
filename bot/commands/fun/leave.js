0b94cc629e0bdbe9787e2e5854af22b94042907b
aaranl <66337295+aaranl@users.noreply.github.com>
2023-07-01 17:45:03 -0400

    commenting

--- bot/commands/fun/leave.js
+++ bot/commands/fun/leave.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { getVoiceConnection } = require('@discordjs/voice');
 
+//I dont really understand how connections work so I need to research the fundamentals of them to understand how they get destroyed
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
