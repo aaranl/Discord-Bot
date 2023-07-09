1284a559f7eb1fbd33400320a2a53c675ceff077
aaranl <66337295+aaranl@users.noreply.github.com>
2023-07-08 22:31:24 -0400

    messing with backend

--- site/backend/index.js
+++ site/backend/index.js
@@ -1,45 +1,89 @@
-const express = require("express")
+const express = require('express');
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const {clientId , clientSecret, callbackURL} = require('../../config.json');
+
 const app = express();
-const cors = require('cors');
-const passport = require("passport");
-const passportFile = require("./config/passport-setup");
-const session = require('express-session');
-const port = process.env.PORT;
+const port = process.env.port;
 
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_ID;
+const redirect_uri = process.env.REDIRECT_URI;
+const secret_key = process.env.SECRET_KEY
 
-//TODO: Rewrite entire backend code to give functionality to the token that is required in OAuth2
-//Look at discord backend documentation
+app.get('/oauth/redirect', (req, res) => {
+    const requestToken = req.query.code;
+    axios({
+        method: 'post',
+        url: `https://discord.com/api/oauth2/token`,
+        data: {
+            client_id,
+            client_secret,
+            grant_type: 'authorization_code',
+            code: requestToken,
+            redirect_uri,
+            scope: ['bot', 'identify']
+        },
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }).then(response => {
+        const accessToken = response.data.access_token;
+        const token = jwt.sign({ accessToken }, secret_key);
 
-app.use(cors({
-    origin: 'https://ezbake.xyz'
-}));
-
-app.use(session ({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get("/auth/discord", passport.authenticate("discord", { permissions: 8 })); //WORKS
-app.get("/auth/discord/callback", passport.authenticate("discord", {
-    failureRedirect: 'https://ezbake.xyz/error' //In the case of a failure redirect the user back to the homepage
-}), function(req, res) {
-    res.redirect("https://ezbake.xyz") //In the case of a success redirect the user to a new page 
+        // You may want to redirect your user to your website's dashboard, sending along the token.
+        res.redirect(`/dashboard?token=${token}`);
+    }).catch(error => {
+        console.error(error);
+        res.status(500).send('Error during Discord OAuth2');
+    });
 });
 
-//Error catching middleware for any errors that aren't handled elsewhere
+app.listen(port, () => console.log(`Server listening at ${port}`));
 
-app.use(function (err, req, res, next) {
-    console.error(err.stack); 
-    res.status(500).send('Something went wrong'); 
-  });
 
-app.listen(port, '0.0.0.0', () => console.log(`Server is running on port 4000`))
-// Debugging env variables
-// console.log(process.env.CLIENT_ID);
-// console.log(process.env.CLIENT_SECRET);
-// console.log(process.env.SESSION_SECRET);
 
+// const express = require("express")
+// const app = express();
+// const cors = require('cors');
+// const passport = require("passport");
+// const passportFile = require("./config/passport-setup");
+// const session = require('express-session');
+// const port = process.env.PORT;
+
+
+// //TODO: Rewrite entire backend code to give functionality to the token that is required in OAuth2
+// //Look at discord backend documentation
+
+// app.use(cors({
+//     origin: 'https://ezbake.xyz'
+// }));
+
+// app.use(session ({
+//     secret: process.env.SESSION_SECRET,
+//     resave: true,
+//     saveUninitialized: true
+// }));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// app.get("/auth/discord", passport.authenticate("discord", { permissions: 8 })); //WORKS
+// app.get("/auth/discord/callback", passport.authenticate("discord", {
+//     failureRedirect: 'https://ezbake.xyz/error' //In the case of a failure redirect the user back to the homepage
+// }), function(req, res) {
+//     res.redirect("https://ezbake.xyz") //In the case of a success redirect the user to a new page 
+// });
+
+// //Error catching middleware for any errors that aren't handled elsewhere
+
+// app.use(function (err, req, res, next) {
+//     console.error(err.stack); 
+//     res.status(500).send('Something went wrong'); 
+//   });
+
+// app.listen(port, '0.0.0.0', () => console.log(`Server is running on port 4000`))
+// // Debugging env variables
+// // console.log(process.env.CLIENT_ID);
+// // console.log(process.env.CLIENT_SECRET);
+// // console.log(process.env.SESSION_SECRET);
