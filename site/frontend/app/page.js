9e728b69948b693708fc1d22e22ac203f0cf8ba0
aaranl <66337295+aaranl@users.noreply.github.com>
2023-07-10 22:52:08 -0400

    attempt 324

--- site/frontend/app/page.js
+++ site/frontend/app/page.js
@@ -4,6 +4,8 @@ import styles from './page.module.css'
 
 //TODO: Make a real frontend website that isnt just a button
 
+//https://api.ezbake.xyz/auth/discord
+
 
 export default function Home() {
   return (
@@ -11,7 +13,7 @@ export default function Home() {
 
       <div className={styles.description}>
         <p>
-          <a href="https://api.ezbake.xyz/auth/discord">Login</a>
+          <a href="https://api.ezbake.xyz/oauth/redirect">Login</a>
         </p>
       </div>
 
