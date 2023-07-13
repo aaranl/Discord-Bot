c810e23add82494ec6ac9781e1051861bbbaebe1
aaranl <66337295+aaranl@users.noreply.github.com>
2023-07-13 19:40:26 -0400

    attempt 9

--- site/frontend/app/page.js
+++ site/frontend/app/page.js
@@ -1,62 +1,46 @@
-import { useEffect } from 'react';
 import styles from './page.module.css';
 
 export default function Home() {
-  useEffect(() => {
-    const handleLogin = () => {
-      // The redirect URI registered with Discord
-      const redirectUri = encodeURIComponent('https://ezbake.xyz/callback');
+  // The redirect URI registered with Discord
+  const redirectUri = encodeURIComponent('https://ezbake.xyz/callback');
 
-      // The client ID of your Discord application
-      const clientId = 976699048134332496;
+  // The client ID of your Discord application
+  const clientId = 976699048134332496;
 
-      // Requesting bot scope with permissions
-      const permissions = 8;
+  // Requesting bot scope with permissions
+  const permissions = 8;
 
-      // The Discord authorization URL
-      const url = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=identify%20bot&permissions=${permissions}`;
-
-      // Redirect to Discord's authorization page
-      window.location.href = url;
-    };
-
-    const loginButton = document.getElementById('login-button');
-    loginButton.addEventListener('click', handleLogin);
-
-    // Cleanup function
-    return () => {
-      loginButton.removeEventListener('click', handleLogin);
-    };
-  }, []);
+  // The Discord authorization URL
+  const url = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=identify%20bot&permissions=${permissions}`;
 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <button id='login-button'>Login</button>
+        <a href={url} className={styles.button}>Login</a>
       </div>
     </main>
   );
 }
 
 
-// import styles from './page.module.css'
 
-// //TODO: Make a real frontend website that isnt just a button
+//TODO: Make a real frontend website that isnt just a button
 
-// //https://api.ezbake.xyz/auth/discord
+//https://api.ezbake.xyz/auth/discord
 
 
-// export default function Home() {
-//   return (
-//     <main className={styles.main}>
+export default function Home() {
+  return (
+    <main className={styles.main}>
 
-//       <div className={styles.description}>
-//         <p>
-//           <a href="https://api.ezbake.xyz/oauth/redirect">Login</a>
+      <div className={styles.description}>
+        <p>
           
-//         </p>
-//       </div>
+          <a href="https://api.ezbake.xyz/oauth/redirect">Login</a>
+          
+        </p>
+      </div>
 
-//     </main>
-//   )
-// }
+    </main>
+  )
+}
