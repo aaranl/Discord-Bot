14214af932023c42792b2d8142f58b0c0804cbe7
aaranl <66337295+aaranl@users.noreply.github.com>
2023-07-15 20:10:17 -0400

    setting out framework for what i need to work on for the frontpage.

--- site/frontend/app/page.js
+++ site/frontend/app/page.js
@@ -5,6 +5,13 @@ export default function Home() {
   // The Discord authorization URL
   const url = "https://discord.com/api/oauth2/authorize?client_id=976699048134332496&permissions=8&redirect_uri=https%3A%2F%2Fapi.ezbake.xyz%2Fauth%2Fdiscord%2Fcallback&response_type=code&scope=identify%20bot";
 
+
+  //Very basic code need to work on this overall to display for numurous things.
+  //Features
+  //Documentation of the commands
+  //Information about the permissions the bot is given, and the information the bot can attain from the server for the user.
+  //TOS
+  
   return (
     <main className={styles.main}>
       <div className={styles.description}>
