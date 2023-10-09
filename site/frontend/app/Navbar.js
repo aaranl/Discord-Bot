b295519e761397810480584ffa5d3889a028347b
aaranl <lawingaaran@gmail.com>
2023-10-08 23:24:56 -0400

    frontend implementation 4

--- site/frontend/app/Navbar.js
+++ site/frontend/app/Navbar.js
@@ -1,16 +1,16 @@
-Import React from 'react';
+// Import React from 'react';
 
 
-const Navbar = () => {
-    return (
-        <nav>
-            <ul>
-                <li><a href ="/">Home</a></li>
-                <li><a hred ="/documentation">About</a></li>
-                <li><a href ="/information"></a></li>
-                <li><a>Login</a></li>
+// const Navbar = () => {
+//     return (
+//         <nav>
+//             <ul>
+//                 <li><a href ="/">Home</a></li>
+//                 <li><a hred ="/documentation">About</a></li>
+//                 <li><a href ="/information"></a></li>
+//                 <li><a>Login</a></li>
 
-            </ul>
-        </nav>
-    )
-}
+//             </ul>
+//         </nav>
+//     )
+// }
