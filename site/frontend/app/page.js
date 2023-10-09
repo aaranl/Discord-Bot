dc6a07737b53378c0c360b4217481fbf0a08dc6e
aaranl <lawingaaran@gmail.com>
2023-10-08 23:39:51 -0400

    frontent implementation 5

--- site/frontend/app/page.js
+++ site/frontend/app/page.js
@@ -1,21 +1,10 @@
 import styles from './page.module.css';
 
 export default function Home() {
-
   // The Discord authorization URL
   const url = "https://discord.com/api/oauth2/authorize?client_id=976699048134332496&permissions=8&redirect_uri=https%3A%2F%2Fapi.ezbake.xyz%2Fauth%2Fdiscord%2Fcallback&response_type=code&scope=identify%20bot";
 
-
-  //Very basic code need to work on this overall to display for numurous things.
-  //Features
-  //Documentation of the commands
-  //Information about the permissions the bot is given, and the information the bot can attain from the server for the user.
-  //TOS
-  //Make it look nice, look nice, feel nice
-  //Look up some guide for CSS i really dont understand how it apply it properly.
-  
   return (
-
     <header className={styles.taskbar}>
       <div className={styles.logo}>
         <h1>Ezbake Bot</h1>
@@ -23,19 +12,17 @@ export default function Home() {
 
       <nav className={styles.navigation}>
         <ul>
-          <li><a href ="/">Home</a></li>
-          <li><a href ="/documentation">About</a></li>
-          <li><a href ="/information"></a>Information</li>
-
+          <li><a href="/">Home</a></li>
+          <li><a href="/documentation">About</a></li>
+          <li><a href="/information">Information</a></li>
         </ul>
       </nav>
-      
+
       <div className={styles.login}>
         <a href={url} className={styles.button}>
           Login
         </a>
       </div>
-
     </header>
   );
 }
