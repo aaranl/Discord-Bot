4d268b61b0d7b3815adc7fb8e4d9fd52b2daae12
aaranl <lawingaaran@gmail.com>
2023-10-10 12:12:13 -0400

    fijefirf

--- site/frontend/app/page.js
+++ site/frontend/app/page.js
@@ -5,8 +5,7 @@ export default function Home() {
   const url = "https://discord.com/api/oauth2/authorize?client_id=976699048134332496&permissions=8&redirect_uri=https%3A%2F%2Fapi.ezbake.xyz%2Fauth%2Fdiscord%2Fcallback&response_type=code&scope=identify%20bot";
 
   return (
-    //<header className={`${styles.taskbar} fade`}>
-    <header className={`${styles.taskbar} item`}>
+    <header className={styles.taskbar}>
 
       <div className={styles.logo}>
         <h1><a href ="/">Ezbake Bot</a></h1>
@@ -14,9 +13,9 @@ export default function Home() {
 
       <nav className={styles.navigation}>
         <ul>
-          <li><a href="/documentation">About</a></li>
-          <li><a href="/information">Information</a></li>
-          <li><a href={url} className={styles.button}>Login</a></li>
+          <li><a href="/documentation" className={styles.taskbar-item}>About</a></li>
+          <li><a href="/information" className={styles.taskbar-item}>Information</a></li>
+          <li><a href={url} className={styles.button} >Login</a></li>
         </ul>
       </nav>
 
