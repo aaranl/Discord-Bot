35d53886017ab09fb9db6ab661e3005ccb508871
aaranl <lawingaaran@gmail.com>
2023-10-10 10:58:17 -0400

    just work bro

--- site/frontend/app/page.js
+++ site/frontend/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   const url = "https://discord.com/api/oauth2/authorize?client_id=976699048134332496&permissions=8&redirect_uri=https%3A%2F%2Fapi.ezbake.xyz%2Fauth%2Fdiscord%2Fcallback&response_type=code&scope=identify%20bot";
 
   return (
-    <header className={styles.taskbar}>
+    <header className={`${styles.taskbar} fade`}>
 
       <div className={styles.logo}>
         <h1><a href ="/">Ezbake Bot</a></h1>
