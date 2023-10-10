8b38799497acebd1e624cfb9aeceec08ef6ab005
aaranl <lawingaaran@gmail.com>
2023-10-10 13:24:39 -0400

    test

--- site/frontend/app/page.js
+++ site/frontend/app/page.js
@@ -5,22 +5,31 @@ export default function Home() {
   const url = "https://discord.com/api/oauth2/authorize?client_id=976699048134332496&permissions=8&redirect_uri=https%3A%2F%2Fapi.ezbake.xyz%2Fauth%2Fdiscord%2Fcallback&response_type=code&scope=identify%20bot";
 
   return (
-    <header className={styles.taskbar}>
+    <div>
+      <header className={styles.taskbar}>
 
-      <div className={styles.logo}>
-        <h1><a href ="/">Ezbake Bot</a></h1>
-      </div>
+        <div className={styles.logo}>
+          <h1><a href ="/">Ezbake Bot</a></h1>
+        </div>
 
-      <nav className={styles.navigation}>
-        <ul>
-          <li><a href="/documentation">About</a></li>
-          <li><a href="/information">Information</a></li>
-          <li><a href={url} className={styles.button} >Login</a></li>
-        </ul>
-      </nav>
+        <nav className={styles.navigation}>
+          <ul>
+            <li><a href="/documentation">About</a></li>
+            <li><a href="/information">Information</a></li>
+            <li><a href={url} className={styles.button} >Login</a></li>
+          </ul>
+        </nav>
 
-    </header> 
-    
+      </header> 
+
+      <main>
+        <div> 
+          <text>awdmwdwd</text>
+
+        </div>
+      </main>
+
+    </div>
 
 
   );
