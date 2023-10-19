bdea1da3055d67b7bb1b96aacd0e3981d7a728cb
Aaran Lawing <66337295+aaranl@users.noreply.github.com>
2023-10-19 18:52:24 -0400

    work on bot

--- bot/deployment/deploy-commands-globally.js
+++ bot/deployment/deploy-commands-globally.js
@@ -34,13 +34,11 @@ const rest = new REST().setToken(token);
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
 			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
-
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
 		// And of course, make sure you catch and log any errors!
